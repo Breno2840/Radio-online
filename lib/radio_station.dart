5f2ce2871f4d6bb57e568f9ae45b91dfd8074e82
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2025-09-10 11:53:12 -0300

    Criar radio_station.dart

---
+++ lib/radio_station.dart
@@ -0,0 +1,13 @@
+// Define a estrutura de dados para uma estação de rádio
+class RadioStation {
+  final String name;
+  final String streamUrl;
+  final String imageUrl;
+
+  // Construtor da classe
+  RadioStation({
+    required this.name,
+    required this.streamUrl,
+    required this.imageUrl,
+  });
+}
